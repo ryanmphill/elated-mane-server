b9246703ae6a70068cbf417d4b14c74d5f88a508
Ryan Phillips <ryanmphill@gmail.com>
2023-08-04 13:50:26 -0500

    Included clients in request for '/styles'

--- maneapi/models/customer.py
+++ maneapi/models/customer.py
@@ -8,5 +8,5 @@ class Customer(models.Model):
     """Customer model class"""
     stylist = models.ForeignKey(User, on_delete=models.CASCADE, related_name="clients")
     name = models.CharField(max_length=50)
-    style = models.ForeignKey(HairStyle, on_delete=models.CASCADE)
+    style = models.ForeignKey(HairStyle, on_delete=models.CASCADE, related_name="clients")
     date_created = models.DateField(auto_now=True)
