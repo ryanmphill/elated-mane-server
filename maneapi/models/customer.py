169e6103b45cfd6bbc2274035f20b032582588df
Ryan Phillips <ryanmphill@gmail.com>
2023-08-04 13:00:03 -0500

    Added appointments to customers via related name

--- maneapi/models/customer.py
+++ maneapi/models/customer.py
@@ -1,10 +1,13 @@
 """Customer class module"""
 from django.db import models
 from django.contrib.auth.models import User
+from .style import HairStyle
 
 
 class Customer(models.Model):
     """Customer model class"""
     stylist = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
+    style = models.ForeignKey(HairStyle, on_delete=models.CASCADE)
     date_created = models.DateField(auto_now=True)
+    
